9669c228a28e3ef3eb21f9ea4c85ffce4149c77d
Glory0206 <dmb07223@gmail.com>
2025-12-02 12:37:22 +0900

    feat: 생성, 업데이트 날짜 자동 주입

--- src/main/java/com/dormclean/dorm_cleaning_management/DormCleaningManagementApplication.java
+++ src/main/java/com/dormclean/dorm_cleaning_management/DormCleaningManagementApplication.java
@@ -2,8 +2,10 @@ package com.dormclean.dorm_cleaning_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DormCleaningManagementApplication {
 
 	public static void main(String[] args) {
