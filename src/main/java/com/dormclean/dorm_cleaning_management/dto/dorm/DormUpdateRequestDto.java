df952eb06a2e1e35d24cfe4a6f7af058ed5b9d08
Glory0206 <dmb07223@gmail.com>
2025-12-13 00:37:03 +0900

    feat: dorm 도메인 관련 dto validation 추가 및 예외처리

--- src/main/java/com/dormclean/dorm_cleaning_management/dto/dorm/DormUpdateRequestDto.java
+++ src/main/java/com/dormclean/dorm_cleaning_management/dto/dorm/DormUpdateRequestDto.java
@@ -1,5 +1,10 @@
 package com.dormclean.dorm_cleaning_management.dto.dorm;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record DormUpdateRequestDto(
-                String dormCode) {
+        @Pattern(regexp = "^[0-9]+$", message = "기숙사 코드는 숫자만 입력 가능합니다.")
+        @Size(min = 1, max = 3, message = "기숙사 코드는 3자리 숫자여야 합니다.")
+        String dormCode) {
 }
