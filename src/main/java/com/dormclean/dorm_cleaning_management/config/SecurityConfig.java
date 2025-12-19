b30ef317007974b2291b24bb2d93359b6e2ec0d3
Seokyun <jangsy0623@naver.com>
2025-12-19 09:51:05 +0900

    feat : 계정 관리 페이지 접속 제한

--- src/main/java/com/dormclean/dorm_cleaning_management/config/SecurityConfig.java
+++ src/main/java/com/dormclean/dorm_cleaning_management/config/SecurityConfig.java
@@ -19,11 +19,11 @@ public class SecurityConfig {
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
                 http
                                 .authorizeHttpRequests(auth -> auth
-                                                // .requestMatchers("/check", "/login", "/loginProc")
-                                                .requestMatchers("/**")
+                                                .requestMatchers("/check", "/login", "/loginProc")
+                                                // .requestMatchers("/**")
                                                 .permitAll()
-                                                .requestMatchers("/admin/**").hasAnyRole("ADMIN", "SUPERADMIN")
                                                 .requestMatchers("/admin/account-manager").hasAnyRole("SUPERADMIN")
+                                                .requestMatchers("/admin/**").hasAnyRole("ADMIN", "SUPERADMIN")
                                                 .anyRequest().authenticated())
                                 .csrf(csrf -> csrf.disable())
                                 .formLogin(form -> form
