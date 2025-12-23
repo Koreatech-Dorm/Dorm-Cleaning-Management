1eaba001df4e84873952827a94216772e119f6b6
Glory0206 <dmb07223@gmail.com>
2025-12-23 22:31:32 +0900

    chore: host 8080

--- src/main/java/com/dormclean/dorm_cleaning_management/service/qr/QrCodeServiceImpl.java
+++ src/main/java/com/dormclean/dorm_cleaning_management/service/qr/QrCodeServiceImpl.java
@@ -68,7 +68,12 @@ public class QrCodeServiceImpl implements QrCodeService {
         }
 
         // URL 생성
-        String content = String.format("%s/?token=%s", host, qrCode.getUuid());
+        String content;
+        if(host.contains("8080")){
+            content = String.format("%s/check?token=%s", host, qrCode.getUuid());
+        } else{
+            content = String.format("%s/?token=%s", host, qrCode.getUuid());
+        }
 
         String labelText = String.format("%s동 %s호", dto.dormCode(), dto.roomNumber());
 
